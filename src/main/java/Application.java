bcd2701b6dca3ac95478abba2afd4ea0dd4b69bd
zhangmingming <zhangmingming@qq.com>
2019-06-24 15:27:50 +0800

    springboot 增加定时任务和邮件的test

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.fire.util.MyMapper;
@@ -13,6 +14,7 @@ import com.fire.util.MyMapper;
 //启注解事务管理
 @EnableTransactionManagement  // 启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
 @MapperScan(basePackages = "com.fire.dao", markerInterface = MyMapper.class)
+@EnableScheduling
 public class Application {
      public static void main(String args[]){
     	 
